9a9a1e604a30c317974cbd247e7fef2d1648799c
Jayant Parashar <135538306+Jayantparashar10@users.noreply.github.com>
2024-01-14 20:45:52 +0530

    Update prerequirement .py

--- sds.py
+++ sds.py
@@ -1,2 +1,11 @@
+Youu need to install these module using pip from cmd or terminal 
 import pyttsx3
-print(pyttsx3.__dict__)
+import speeachrecogination
+import wikipedia 
+import pyaudio 
+import webbrowser 
+import Appopner 
+import datetime
+import pyjokes
+You jus need to import these in you code editor by using comand "import"
+import random
