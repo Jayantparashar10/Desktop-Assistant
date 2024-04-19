d0dfd2b222f7d6a4fb6e4e188b646a6b13f0f4e7
Jayant Parashar <135538306+Jayantparashar10@users.noreply.github.com>
2024-04-19 22:44:24 +0530

    Spelling error sds.py

--- sds.py
+++ sds.py
@@ -1,4 +1,4 @@
-Youu need to install these module using pip from cmd or terminal 
+You need to install these module using pip from cmd or terminal 
 import pyttsx3
 import speeachrecogination
 import wikipedia 
@@ -7,5 +7,6 @@ import webbrowser
 import Appopner 
 import datetime
 import pyjokes
-You jus need to import these in you code editor by using comand "import"
 import random
+
+You just need to import these in you code editor by using comand "import"
